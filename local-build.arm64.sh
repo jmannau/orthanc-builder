9db15bf970072bd111617610a94f9c4fa622ac61
James Manners <james@binary.com.au>
2023-03-13 11:32:56 +1100

    arm64

--- local-build.arm64.sh
+++ local-build.arm64.sh
@@ -14,13 +14,8 @@ source bash-helpers.sh
 
 # default arg values
 version=stable
-skipCommitChecks=0
+skipCommitChecks=1
 platform=linux/arm64
-type=local
-step=build
-currentTag=current
-pushTag=unknown
-image=normal
 
 for argument in "$@"; do
     key=$(echo $argument | cut -f1 -d=)
@@ -65,124 +60,45 @@ ORTHANC_OE2_VERSION=$(getBranchTagToBuildDocker "Orthanc-explorer-2" $version)
 BASE_DEBIAN_IMAGE=bullseye-20230202-slim
 BASE_BUILDER_IMAGE_TAG=$BASE_DEBIAN_IMAGE-$version
 
-# list all intermediate targets.  It allows us to "slow down" the build and see what's going wrong (which is not possible with 10 parallel builds)
-# buildTargets="build-orthanc build-gdcm build-plugin-pg build-plugin-mysql build-plugin-transfers build-plugin-dicomweb build-plugin-wsi build-plugin-owv build-plugin-auth build-plugin-python build-plugin-odbc build-plugin-indexer build-plugin-neuro build-plugin-tcia build-stone-viewer build-s3-object-storage build-oe2"
-buildTargets="build-orthanc build-gdcm build-plugin-pg build-plugin-mysql build-plugin-transfers build-plugin-dicomweb build-plugin-wsi build-plugin-owv build-plugin-auth build-plugin-python build-plugin-odbc build-plugin-indexer build-plugin-neuro build-plugin-tcia build-s3-object-storage build-oe2"
-
-# by default, we try to build only the normal image (oposed to the full image with vcpkg and MSSQL drivers)
-finalImageTarget=orthanc-no-vcpkg
-
-buildTargets="$buildTargets $finalImageTarget"
-
-# to debug a particular build, you can hardcode the target hereunder (don't commit that !)
-# buildTargets=build-plugin-neuro
-
-if [[ $type == "local" ]]; then
-    from_cache_arg_runner_base=
-    to_cache_arg_runner_base=
-
-    from_cache_arg_builder_base=
-    to_cache_arg_builder_base=
-
-    from_cache_arg=
-    to_cache_arg=
-
-    # when building locally, use Docker builder (easier to reuse local images)
-    build="build"
-    push_load_arg=
-else
-    from_cache_arg_runner_base="--cache-from=osimis/orthanc-runner-base:cache-$BASE_BUILDER_IMAGE_TAG"
-    to_cache_arg_runner_base="--cache-to=osimis/orthanc-runner-base:cache-$BASE_BUILDER_IMAGE_TAG"
-
-    from_cache_arg_builder_base="--cache-from=osimis/orthanc-builder-base:cache-$BASE_BUILDER_IMAGE_TAG"
-    to_cache_arg_builder_base="--cache-to=osimis/orthanc-builder-base:cache-$BASE_BUILDER_IMAGE_TAG"
-
-    from_cache_arg="--cache-from=osimis/orthanc-builder-base:cache-main-$BASE_BUILDER_IMAGE_TAG"
-    to_cache_arg="--cache-to=osimis/orthanc-builder-base:cache-main-$BASE_BUILDER_IMAGE_TAG"
-
-    # when building in CI, use buildx
-    build="buildx build"
-    push_load_arg="--push"
-
-    # when building in CI, don't use intermediate targets (it would push plenty of images)
-    buildTargets=$finalImageTarget
-fi
-
-if [[ $step == "push" ]]; then
-
-    if [[ $version == "unstable" ]]; then
-        final_tag=$pushTag-unstable
-    else
-        final_tag=$pushTag
-    fi
-
-    # tag previously build images and push them
-    docker tag osimis/orthanc:$currentTag osimis/orthanc:$final_tag
-    docker push osimis/orthanc:$final_tag
-    exit 0
-else
-
-    final_tag=$currentTag
-
-fi
-
-# runner_base_tag=$final_image_temporary_tag
-# builder_base_tag=$final_image_temporary_tag
-
 ###### runner-base
-docker $build \
+# buildx build --load to load the built contains to the local docker deamon
+docker build \
     --progress=plain --platform=$platform -t osimis/orthanc-runner-base:$BASE_BUILDER_IMAGE_TAG \
     --build-arg BASE_DEBIAN_IMAGE=$BASE_DEBIAN_IMAGE \
-    $from_cache_arg_runner_base \
-    $to_cache_arg_runner_base \
-    $push_load_arg \
     -f docker/orthanc/Dockerfile.runner-base docker/orthanc
 
 ###### builder-base
-docker $build \
+# buildx build to load the built contains to the local docker deamon
+docker build --pull=false \
     --progress=plain --platform=$platform -t osimis/orthanc-builder-base:$BASE_BUILDER_IMAGE_TAG \
-    $from_cache_arg_builder_base \
-    $to_cache_arg_builder_base \
-    $push_load_arg \
     --build-arg BASE_IMAGE_TAG=$BASE_BUILDER_IMAGE_TAG \
     -f docker/orthanc/Dockerfile.builder-base docker/orthanc
 
-for target in $buildTargets; do
+target='osimis/orthanc'
 
-    if [[ $target == $finalImageTarget ]]; then
-        tag_arg="--tag osimis/orthanc:$final_tag"
-    else
-        tag_arg=
-    fi
-
-    # sleep 5
-    ###### osimis/orthanc
-    docker $build \
-        --progress=plain --platform=$platform \
-        --build-arg ORTHANC_COMMIT_ID=$ORTHANC_COMMIT_ID \
-        --build-arg ORTHANC_GDCM_COMMIT_ID=$ORTHANC_GDCM_COMMIT_ID \
-        --build-arg ORTHANC_PG_COMMIT_ID=$ORTHANC_PG_COMMIT_ID \
-        --build-arg ORTHANC_MYSQL_COMMIT_ID=$ORTHANC_MYSQL_COMMIT_ID \
-        --build-arg ORTHANC_TRANSFERS_COMMIT_ID=$ORTHANC_TRANSFERS_COMMIT_ID \
-        --build-arg ORTHANC_DW_COMMIT_ID=$ORTHANC_DW_COMMIT_ID \
-        --build-arg ORTHANC_WSI_COMMIT_ID=$ORTHANC_WSI_COMMIT_ID \
-        --build-arg ORTHANC_OWV_COMMIT_ID=$ORTHANC_OWV_COMMIT_ID \
-        --build-arg ORTHANC_AUTH_COMMIT_ID=$ORTHANC_AUTH_COMMIT_ID \
-        --build-arg ORTHANC_PYTHON_COMMIT_ID=$ORTHANC_PYTHON_COMMIT_ID \
-        --build-arg ORTHANC_ODBC_COMMIT_ID=$ORTHANC_ODBC_COMMIT_ID \
-        --build-arg ORTHANC_INDEXER_COMMIT_ID=$ORTHANC_INDEXER_COMMIT_ID \
-        --build-arg ORTHANC_NEURO_COMMIT_ID=$ORTHANC_NEURO_COMMIT_ID \
-        --build-arg ORTHANC_TCIA_COMMIT_ID=$ORTHANC_TCIA_COMMIT_ID \
-        --build-arg ORTHANC_STONE_VIEWER_COMMIT_ID=$ORTHANC_STONE_VIEWER_COMMIT_ID \
-        --build-arg ORTHANC_AWS_STORAGE_COMMIT_ID=$ORTHANC_AWS_STORAGE_COMMIT_ID \
-        --build-arg ORTHANC_OE2_COMMIT_ID=$ORTHANC_OE2_COMMIT_ID \
-        --build-arg ORTHANC_OE2_VERSION=$ORTHANC_OE2_VERSION \
-        --build-arg BASE_IMAGE_TAG=$BASE_BUILDER_IMAGE_TAG \
-        $from_cache_arg \
-        $to_cache_arg \
-        $push_load_arg \
-        $tag_arg \
-        --target $target \
-        -f docker/orthanc/Dockerfile.arm64 docker/orthanc/
-
-done
+# sleep 5
+###### osimis/orthanc
+# buildx build to load the built contains to the local docker deamon
+docker build \
+    --progress=plain --platform=$platform \
+    --build-arg ORTHANC_COMMIT_ID=$ORTHANC_COMMIT_ID \
+    --build-arg ORTHANC_GDCM_COMMIT_ID=$ORTHANC_GDCM_COMMIT_ID \
+    --build-arg ORTHANC_PG_COMMIT_ID=$ORTHANC_PG_COMMIT_ID \
+    --build-arg ORTHANC_MYSQL_COMMIT_ID=$ORTHANC_MYSQL_COMMIT_ID \
+    --build-arg ORTHANC_TRANSFERS_COMMIT_ID=$ORTHANC_TRANSFERS_COMMIT_ID \
+    --build-arg ORTHANC_DW_COMMIT_ID=$ORTHANC_DW_COMMIT_ID \
+    --build-arg ORTHANC_WSI_COMMIT_ID=$ORTHANC_WSI_COMMIT_ID \
+    --build-arg ORTHANC_OWV_COMMIT_ID=$ORTHANC_OWV_COMMIT_ID \
+    --build-arg ORTHANC_AUTH_COMMIT_ID=$ORTHANC_AUTH_COMMIT_ID \
+    --build-arg ORTHANC_PYTHON_COMMIT_ID=$ORTHANC_PYTHON_COMMIT_ID \
+    --build-arg ORTHANC_ODBC_COMMIT_ID=$ORTHANC_ODBC_COMMIT_ID \
+    --build-arg ORTHANC_INDEXER_COMMIT_ID=$ORTHANC_INDEXER_COMMIT_ID \
+    --build-arg ORTHANC_NEURO_COMMIT_ID=$ORTHANC_NEURO_COMMIT_ID \
+    --build-arg ORTHANC_TCIA_COMMIT_ID=$ORTHANC_TCIA_COMMIT_ID \
+    --build-arg ORTHANC_STONE_VIEWER_COMMIT_ID=$ORTHANC_STONE_VIEWER_COMMIT_ID \
+    --build-arg ORTHANC_AWS_STORAGE_COMMIT_ID=$ORTHANC_AWS_STORAGE_COMMIT_ID \
+    --build-arg ORTHANC_OE2_COMMIT_ID=$ORTHANC_OE2_COMMIT_ID \
+    --build-arg ORTHANC_OE2_VERSION=$ORTHANC_OE2_VERSION \
+    --build-arg BASE_IMAGE_TAG=$BASE_BUILDER_IMAGE_TAG \
+    --tag osimis/orthanc:latest \
+    -f docker/orthanc/Dockerfile.arm64 docker/orthanc/
